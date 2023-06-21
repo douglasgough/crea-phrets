8c280b48737c30eb40e11c0a31c7535f40e497ab
Douglas Gough <dougwgough@gmail.com>
2023-06-21 16:31:21 -0700

    Add example code to pull 100 listing IDs and display on a Drupal page

--- src/Controller/CREARetsSearchController.php
+++ src/Controller/CREARetsSearchController.php
@@ -4,10 +4,9 @@ namespace Drupal\crearets\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use PHRETS\Configuration;
 use PHRETS\Session;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Controller for the search page.
@@ -15,98 +14,87 @@ use Symfony\Component\HttpFoundation\Response;
 class CREARetsSearchController extends ControllerBase {
 
   /**
-   * The PHRETS session.
+   * The configuration factory.
    *
-   * @var \PHRETS\Session
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
-  protected $session;
+  protected $configFactory;
 
   /**
-   * The CREARets configuration.
+   * Constructs a new CustomRetsModuleController object.
    *
-   * @var \Drupal\Core\Config\Config
-   */
-  protected $config;
-
-  /**
-   * Constructs a CREARetsSearchController object.
-   *
-   * @param \PHRETS\Session $session
-   *   The PHRETS session.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The configuration factory.
    */
-  public function __construct(Session $session, ConfigFactoryInterface $config_factory) {
-    $this->session = $session;
-    $this->config = $config_factory->get('crearets.settings');
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
   }
 
   /**
    * {@inheritdoc}
+   *
+   * This is a factory method that returns a new instance of this class.
+   * The factory should pass any needed dependencies into the constructor of this class, but not the container itself.
    */
   public static function create(ContainerInterface $container) {
-    $config = $container->get('config.factory')->get('crearets.settings');
-
-    // Create a PHRETS session and establish the connection using configuration values.
-    $retsConfig = new Configuration;
-    $retsConfig->setLoginUrl($config->get('url'))
-      ->setUsername($config->get('username'))
-      ->setPassword($config->get('password'))
-      ->setRetsVersion('RETS/1.7.2')
-      ->setOption('compression_enabled', TRUE)
-      ->setOption('use_digest_authentication', TRUE)
-      ->setOption('disable_follow_location', TRUE)
-      ->setOption('offset_support', TRUE)
-      ->setOption('accept_header', '/');
-
-    $session = new Session($retsConfig);
-    $session->login();
-
-    return new static($session, $container->get('config.factory'));
+    $config_factory = $container->get('config.factory');
+    return new static($config_factory);
   }
 
   /**
-   * Returns the search page.
-   *
-   * @return \Symfony\Component\HttpFoundation\Response
-   *   The rendered search page.
+   * Retrieves records from the CREA RETS server using PhRETS.
    */
   public function searchPage() {
-    // Perform RETS queries and process the response.
-    $search = $this->session->Search('Property', 'ResidentialProperty', '(Status=A)');
-    if ($search->isSuccessful()) {
-      $results = $search->getResults();
+    $config = $this->configFactory->get('crearets.settings');
+    $crea_rets_url = $config->get('url');
+    $crea_rets_username = $config->get('username');
+    $crea_rets_password = $config->get('password');
 
-      // Build a table of search results.
-      $header = [
-        'MLS Number',
-        'Price',
-      ];
-      $rows = [];
+    // Configure PhRETS with the login information.
+    $config = new Configuration;
+    $config->setLoginUrl($crea_rets_url);
+    $config->setUsername($crea_rets_username);
+    $config->setPassword($crea_rets_password);
+    $config->setRetsVersion('1.7.2');
 
-      foreach ($results as $result) {
-        $mlsNumber = $result->get('MLSNumber');
-        $price = $result->get('ListPrice');
+    // Create a session and retrieve records.
+    $rets = new Session($config);
+    $rets->Login();
 
-        $rows[] = [
-          $mlsNumber,
-          $price,
-        ];
-      }
+    // Example code to retrieve properties from the CREA RETS server.
+    $resource = 'Property';
+    $dbml = "(ID=*)";
 
-      $table = [
-        '#type' => 'table',
-        '#header' => $header,
-        '#rows' => $rows,
-      ];
+    $results = $rets->Search(
+      $resource,
+      $resource,
+      $dbml,
+      [
+        'QueryType' => 'DMQL2',
+        'Count' => 1, // count and records
+        'Format' => 'COMPACT-DECODED',
+        'Limit' => 100,
+        'StandardNames' => 0, // give system names
+      ]
+    );
 
-      // Return the rendered table.
-      return new Response(\Drupal::service('renderer')->render($table));
-    }
-    else {
-      // Handle search error.
-      return new Response('Error: ' . $search->getError());
-    }
+    $all_ids = $results->lists('ListingKey');
+    // $all_ids now contains an array of ListingKey values from the search results.
+
+    // Close the session.
+    $rets->Logout();
+
+    // Return a render array for the Drupal page.
+    $content = [
+      '#theme' => 'item_list',
+      '#list_type' => 'ul',
+      '#title' => 'My List',
+      '#items' => $all_ids,
+      '#attributes' => ['class' => 'mylist'],
+      '#wrapper_attributes' => ['class' => 'container'],
+    ];
+    return $content;
+
   }
 
 }
