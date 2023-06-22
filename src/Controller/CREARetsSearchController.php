05431a5f1d4d81dea061cefa85c2d45ec164905a
Douglas Gough <dougwgough@gmail.com>
2023-06-22 15:40:45 -0700

    Ditch PHRETS 2.x and use PHRETS 1.0 instead. Include PHRETS inside module as a class. Because CREA RETS server returns XML that PHRETS 2.x does not understand.

--- src/Controller/CREARetsSearchController.php
+++ src/Controller/CREARetsSearchController.php
@@ -5,8 +5,7 @@ namespace Drupal\crearets\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use PHRETS\Configuration;
-use PHRETS\Session;
+use Drupal\crearets\CreaPhrets;
 
 /**
  * Controller for the search page.
@@ -35,6 +34,10 @@ class CREARetsSearchController extends ControllerBase {
    *
    * This is a factory method that returns a new instance of this class.
    * The factory should pass any needed dependencies into the constructor of this class, but not the container itself.
+   *
+   * I'm passing the ContainerInterface into create() and returning the config.factory service. The service is passed into
+   * the __construct method so that I have access to the config values inside the instantiated Controller. This is
+   * dependency injection.
    */
   public static function create(ContainerInterface $container) {
     $config_factory = $container->get('config.factory');
@@ -43,53 +46,65 @@ class CREARetsSearchController extends ControllerBase {
 
   /**
    * Retrieves records from the CREA RETS server using PhRETS.
+   *
+   * configFactory is the injected dependency
    */
+
+//  TODO Refactor to create a service that handles login, logout and session. Include error handling and firewall test.
   public function searchPage() {
+    $rets = new CREAPhrets();
     $config = $this->configFactory->get('crearets.settings');
     $crea_rets_url = $config->get('url');
     $crea_rets_username = $config->get('username');
     $crea_rets_password = $config->get('password');
 
     // Configure PhRETS with the login information.
+    /**
     $config = new Configuration;
     $config->setLoginUrl($crea_rets_url);
     $config->setUsername($crea_rets_username);
     $config->setPassword($crea_rets_password);
     $config->setRetsVersion('1.7.2');
-
+    $config->setOption('compression_enabled', true);
+    $config->setOption('disable_follow_location', true);
+    $config->setOption('offset_support', true);
+    $config->setOption('Accept','/');
+    */
     // Create a session and retrieve records.
-    $rets = new Session($config);
-    $rets->Login();
-
-    // Example code to retrieve properties from the CREA RETS server.
+    //$rets = new Session($config);
+    $rets->Connect($crea_rets_url,$crea_rets_username,$crea_rets_password);
+    // Get the master list from the CREA RETS server.
+//    TODO Create a configuration form for Search parameter
     $resource = 'Property';
     $dbml = "(ID=*)";
-
-    $results = $rets->Search(
+    $results = $rets->SearchQuery(
       $resource,
       $resource,
       $dbml,
       [
         'QueryType' => 'DMQL2',
-        'Count' => 1, // count and records
-        'Format' => 'COMPACT-DECODED',
-        'Limit' => 100,
-        'StandardNames' => 0, // give system names
+        'Count' => 0, // count and records
+        'Format' => 'STANDARD-XML',
+        'Limit' => 1,
+        //'StandardNames' => 0, // give system names
       ]
     );
-
-    $all_ids = $results->lists('ListingKey');
+// TODO Get all available listings with date and id number. Include error handling.
+    //$all_ids = $results[''];
     // $all_ids now contains an array of ListingKey values from the search results.
+    // $count is the number of listings available using the configured query
+    //$count[] = $results->count();
+    //$response[] = $rets->getLastResponse();
 
     // Close the session.
-    $rets->Logout();
-
+    $rets->Disconnect();
+    kpr($results);
     // Return a render array for the Drupal page.
     $content = [
       '#theme' => 'item_list',
       '#list_type' => 'ul',
       '#title' => 'My List',
-      '#items' => $all_ids,
+      '#items' => $results,
       '#attributes' => ['class' => 'mylist'],
       '#wrapper_attributes' => ['class' => 'container'],
     ];
